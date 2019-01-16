434ca50f64c0c85bb09fb096a4976bb03fb80251
Brarian <b.namonywa@gmail.com>
2019-01-16 14:19:17 -0600

    Updates

---
+++ precache-manifest.cc742682ddb99726ffddfcc0495e15a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "01ef914ceaa9c48c97f340344e45be02",
+    "url": "/portfolio2018/static/media/shipped.01ef914c.png"
+  },
+  {
+    "revision": "949dd586def82ede42186bf1715e5bd9",
+    "url": "/portfolio2018/static/media/littlemixer.949dd586.png"
+  },
+  {
+    "revision": "59cac06b680c034423da954b4fd08206",
+    "url": "/portfolio2018/static/media/kepShowNotes.59cac06b.png"
+  },
+  {
+    "revision": "dd9e3bb4cd9ca6c5e410fe19a7f00047",
+    "url": "/portfolio2018/static/media/kepHome.dd9e3bb4.png"
+  },
+  {
+    "revision": "313c9bbdf8c577a272607f618cb2c205",
+    "url": "/portfolio2018/static/media/austin1.313c9bbd.jpg"
+  },
+  {
+    "revision": "6f56b5e127c97503effb",
+    "url": "/portfolio2018/static/js/runtime~main.6f56b5e1.js"
+  },
+  {
+    "revision": "809534cc10c50bda00ae",
+    "url": "/portfolio2018/static/js/main.809534cc.chunk.js"
+  },
+  {
+    "revision": "5240d0c78698c6495b6c",
+    "url": "/portfolio2018/static/js/1.5240d0c7.chunk.js"
+  },
+  {
+    "revision": "809534cc10c50bda00ae",
+    "url": "/portfolio2018/static/css/main.431056bb.chunk.css"
+  },
+  {
+    "revision": "d4dfaab1cef313180dd627d4c4296e51",
+    "url": "/portfolio2018/index.html"
+  }
+];
