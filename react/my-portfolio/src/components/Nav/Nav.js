d01fc3ed398cc996cbe8dc67d78a6ec2885e8613
Brarian <b.namonywa@gmail.com>
2019-01-19 20:55:09 -0600

    homepage added in package.json

--- react/my-portfolio/src/components/Nav/Nav.js
+++ react/my-portfolio/src/components/Nav/Nav.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
-import { Route, NavLink, BrowserRouter } from "react-router-dom";
+import { Route, NavLink,  Router } from "react-router-dom";
+
+import createHashHistory from 'history/createHashHistory'; 
 import About from "../About/About";
 import MyWork from "../MyWork/MyWork";
 import Contact from "../Contact/Contact";
 import "./Nav.css";
 
-
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL }); 
 
 class Nav extends Component {
   render() {
     return (
-      <BrowserRouter basename="/">
+      <Router history={hashHistory}>
         <div className={Nav.css}>
           <header>
             <ul>
               <li className="logo">Brarian</li>
               <li>
-                <NavLink to="/home">About</NavLink>
+                <NavLink to="/">About</NavLink>
               </li>
               <li>
                 <NavLink to="/work">My Work</NavLink>
@@ -27,11 +29,11 @@ class Nav extends Component {
             </ul>
           </header>
           <main className="content" />
-          <Route exact path="/about" component={About} />
+          <Route exact path="/" component={About} />
           <Route path="/work" component={MyWork} />
           <Route path="/contact" component={Contact} />
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 
